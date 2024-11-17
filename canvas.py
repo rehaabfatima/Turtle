801fbc547e3524a1cef3fe8cb4d39b12d8509c43
rehaab fatima <rehaabfatima628@gmail.comm>
2024-11-17 18:36:55 +0500

    add

---
+++ canvas.py
@@ -0,0 +1,19 @@
+import turtle
+
+# creating screen
+screen = turtle.Screen()
+
+
+# screen size
+screen.setup(width=800,height=600)
+
+# changing background color
+screen.bgcolor("blue")
+
+# creating a shape
+wn= turtle.shape("turtle")
+turtle.color("green")
+
+
+# setting up screen 
+screen.mainloop()
